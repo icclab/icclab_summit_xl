90e0864f7ee67e4aea7273f6a1a6603fb91a14d6
Giovanni Toffetti <giovanni.toffetti@gmail.com>
2018-11-27 13:26:09 +0100

    added schunk pg70. dependency from https://github.com/ipa320/schunk_modular_robotics

--- scripts/send_gripper.py
+++ scripts/send_gripper.py
@@ -13,22 +13,30 @@ import control_msgs.msg
 def gripper_client(value):
 
     # Create an action client
-    client = actionlib.SimpleActionClient(
-        '/summit_xl/gripper_controller/gripper_cmd',  # namespace of the action topics
+    client_l = actionlib.SimpleActionClient(
+        '/summit_xl/gripper_left_controller/gripper_cmd',  # namespace of the action topics
+        control_msgs.msg.GripperCommandAction # action type
+    )
+    
+    client_r = actionlib.SimpleActionClient(
+        '/summit_xl/gripper_right_controller/gripper_cmd',  # namespace of the action topics
         control_msgs.msg.GripperCommandAction # action type
     )
     
     # Wait until the action server has been started and is listening for goals
-    client.wait_for_server()
+    client_l.wait_for_server()
+    client_r.wait_for_server()
 
     # Create a goal to send (to the action server)
     goal = control_msgs.msg.GripperCommandGoal()
     goal.command.position = value   # From 0.0 to 0.8
     goal.command.max_effort = -1.0  # Do not limit the effort
-    client.send_goal(goal)
+    client_l.send_goal(goal)
+    client_r.send_goal(goal)
 
-    client.wait_for_result()
-    return client.get_result()
+    client_l.wait_for_result()
+    client_r.wait_for_result()
+    return client_l.get_result()
 
 
 if __name__ == '__main__':
@@ -36,7 +44,7 @@ if __name__ == '__main__':
         # Get the angle from the command line
         parser = argparse.ArgumentParser()
         parser.add_argument("--value", type=float, default="0.2",
-                            help="Value betwewen 0.0 (open) and 0.8 (closed)")
+                            help="Value betwewen 0.2 (open) 0 (closed)")
         args = parser.parse_args()
         gripper_value = args.value
         # Start the ROS node
