3b11866f0c666e808080e3b5ba619ef2b8d7f24e
Giovanni Toffetti <giovanni.toffetti@gmail.com>
2018-12-03 16:14:41 +0100

    changed gripper to used effort based control

---
+++ scripts/send_effort_gripper.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Send a value to change the opening of an effort controlled gripper
+#
+
+if [ "$#" -ne 1 ]
+  then echo "usage: send_effort_gripper.sh <effort_float_value>  # where positive values open the gripper, negative values close it"
+else
+  rostopic pub -1 /summit_xl/gripper_left_controller/command std_msgs/Float64 -- $1 &
+  rostopic pub -1 /summit_xl/gripper_right_controller/command std_msgs/Float64 -- $1
+fi
