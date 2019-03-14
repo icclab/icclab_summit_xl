e8d9604b7e505a33ead13d4d5a5370c45cd0ac47
rdesc <rodrigue.de.schaetzen@hotmail.com>
2019-03-14 15:20:05 +0100

    adds move_base action client to test navigation in sim

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -1,11 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
 export IMAGE_NAME=robopaas/rosdocked-kinetic-workspace-included:latest
 
 set -e
 
-docker run -d -it $IMAGE_NAME
+docker run\
+  -h `hostname`\
+  --privileged\
+  --net=host\
+  -e SHELL\
+  -e DISPLAY=:0\
+  -e DOCKER=1\
+  -v "/tmp/.X11-unix:/tmp/.X11-unix:rw"\
+  -it -d $IMAGE_NAME $SHELL
+
 export CONTAINER_NAME=$(docker ps --latest --format "{{.Names}}")
 echo $CONTAINER_NAME
-docker exec $CONTAINER_NAME bash -c "cd ~/catkin_ws/src/icclab_summit_xl/; git pull; . /opt/ros/kinetic/setup.bash; . ~/catkin_ws/devel/setup.bash; env | grep ROS; roslaunch icclab_summit_xl irlab_sim_summit_xls_amcl.launch launch_rviz_nav:=false gazebo_gui:=false"
+
+# bringup sim complete with amcl and without rviz or gazebo gui
+docker exec -d $CONTAINER_NAME bash -c "cd ~/catkin_ws/src/icclab_summit_xl/; git pull; . /opt/ros/kinetic/setup.bash; . ~/catkin_ws/devel/setup.bash; roslaunch icclab_summit_xl irlab_sim_summit_xls_amcl.launch launch_rviz_nav:=false gazebo_gui:=false"
+
+# launch move base action client
+docker exec $CONTAINER_NAME bash -c ". /opt/ros/kinetic/setup.bash; . ~/catkin_ws/devel/setup.bash; ROS_NAMESPACE=summit_xl rosrun icclab_summit_xl move_base_client.py"
 
 exit 0
