b707bf37465107439b1c49ddfae0b8cc59a86614
rdesc <rodrigue.de.schaetzen@hotmail.com>
2019-03-15 14:28:29 +0100

    small fix

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -20,10 +20,10 @@ echo $CONTAINER_NAME
 # bringup sim complete with amcl and move_base action client
 echo "Testing navigation stack..."
 echo "Executing 'roslaunch icclab_summit_xl irlab_sim_summit_xls_amcl.launch launch_rviz_nav:=false gazebo_gui:=false nav_test:=true' inside container $CONTAINER_NAME..."
-docker exec -i $CONTAINER_NAME ~/catkin_ws/src/icclab_summit_xl/.ci/nav_test.sh
+docker exec -i $CONTAINER_NAME ./catkin_ws/src/icclab_summit_xl/.ci/nav_test.sh
 
 # extract logs of movebase_client_py node
-docker exec -i $CONTAINER_NAME ~/catkin_ws/src/icclab_summit_xl/.ci/nav_test_get_output.sh >> movebase_client_py_log.txt
+docker exec -i $CONTAINER_NAME ./catkin_ws/src/icclab_summit_xl/.ci/nav_test_get_output.sh >> movebase_client_py_log.txt
 nav_test_output=$(cat movebase_client_py_log.txt)
 
 if [[ "$nav_test_output" == *"fail"* ]] ; then
