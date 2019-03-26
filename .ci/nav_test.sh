01047d7db0bce44217340193a285b72cba6886f8
rdesc <rodrigue.de.schaetzen@hotmail.com>
2019-03-26 13:05:33 +0100

    fixes issue with nav test script hanging after killed

--- .ci/nav_test.sh
+++ .ci/nav_test.sh
@@ -2,14 +2,8 @@
 #
 # test navigation stack in sim
 
-set -e
-
-cd ~/catkin_ws/src/icclab_summit_xl/
-git pull
 . /opt/ros/kinetic/setup.bash
 . ~/catkin_ws/devel/setup.bash
-cd ~/catkin_ws
-catkin build
 roslaunch icclab_summit_xl irlab_summit_xl_amcl.launch launch_rviz_nav:=false gazebo_gui:=false nav_test:=true
 
 exit 0
