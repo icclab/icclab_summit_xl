cd6700d5b27678c3756864c9ccd1d7cda8f3a881
rdesc <rodrigue.de.schaetzen@hotmail.com>
2019-03-26 09:12:28 +0100

    changes nav test to correct amcl launch

--- .ci/nav_test.sh
+++ .ci/nav_test.sh
@@ -9,7 +9,7 @@ git pull
 . /opt/ros/kinetic/setup.bash
 . ~/catkin_ws/devel/setup.bash
 cd ~/catkin_ws
-catkin_make
-roslaunch icclab_summit_xl irlab_sim_summit_xls_amcl.launch launch_rviz_nav:=false gazebo_gui:=false nav_test:=true
+catkin build
+roslaunch icclab_summit_xl irlab_summit_xl_amcl.launch launch_rviz_nav:=false gazebo_gui:=false nav_test:=true
 
 exit 0
