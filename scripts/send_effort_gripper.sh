865b30d8702e9bc813cdddb1f0a4134f5817b172
Georgios Leandros Kyriazis <leankyr@gmail.com>
2020-02-12 10:35:16 +0000

    gripper fixed and renders as it should on gazebo and rviz

--- scripts/send_effort_gripper.sh
+++ scripts/send_effort_gripper.sh
@@ -4,8 +4,8 @@
 #
 
 if [ "$#" -ne 1 ]
-  then echo "usage: send_effort_gripper.sh <effort_float_value>  # where positive values open the gripper, negative values close it"
+  then echo "usage: send_effort_gripper.sh <effort_float_value>  # where positive values close the gripper, negative values open it"
 else
   rostopic pub -1 /summit_xl/gripper_left_controller/command std_msgs/Float64 -- $1 &
-  rostopic pub -1 /summit_xl/gripper_right_controller/command std_msgs/Float64 -- $1
+  #rostopic pub -1 /summit_xl/gripper_right_controller/command std_msgs/Float64 -- $1
 fi
