8d6a86e71a482578637d2df383ad6fb375d44216
Giovanni Toffetti <toff@zhaw.ch>
2021-10-14 14:11:01 +0200

    updated travis build for noetic

--- .ci/docker_push.sh
+++ .ci/docker_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push robopaas/rosdocked-kinetic-workspace-included:latest
+docker push robopaas/rosdocked-noetic-workspace-included:auto
