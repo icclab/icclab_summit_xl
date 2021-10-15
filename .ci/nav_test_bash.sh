6b4a1c017192e347e7ff7315ed97b2ecae5858ed
Giovanni Toffetti <toff@zhaw.ch>
2021-10-15 10:35:05 +0200

    Add sourcing of ROS env

--- .ci/nav_test_bash.sh
+++ .ci/nav_test_bash.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+# source ROS env
+. ~/catkin_ws/devel/setup.bash
+
 # start complete in headless mode 
 roslaunch icclab_summit_xl irlab_sim_summit_xls_complete.launch gazebo_gui:=false launch_rviz_nav:=false launch_rviz_grasping:=false & 
+
 # save PID of roslaunch
 ROSLAUNCH_PID=$!
 
