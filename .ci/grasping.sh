e55d422cc2bfff8e872430dfd996477d58286c68
Leonardo Militano <39118873+miltzhaw@users.noreply.github.com>
2021-12-22 13:50:05 +0100

    Update grasping.sh

--- .ci/grasping.sh
+++ .ci/grasping.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-. /opt/ros/kinetic/setup.bash
+. /opt/ros/noetic/setup.bash
 . ~/catkin_ws/devel/setup.bash
 roslaunch icclab_summit_xl irlab_sim_summit_xls_grasping.launch launch_rviz_grasping:=false gazebo_gui:=false
 
