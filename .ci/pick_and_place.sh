6f180cfd42a2768841848db70faaaaa6ea2d0187
Leonardo Militano <39118873+miltzhaw@users.noreply.github.com>
2021-12-22 13:50:30 +0100

    Update pick_and_place.sh

--- .ci/pick_and_place.sh
+++ .ci/pick_and_place.sh
@@ -4,9 +4,9 @@
 
 set -e
 
-. /opt/ros/kinetic/setup.bash
+. /opt/ros/noetic/setup.bash
 . ~/catkin_ws/devel/setup.bash
 cd ~/catkin_ws/src/icclab_summit_xl/scripts/
-printf 'y\ny\ny\n' | python pick_and_place_summit_simulation.py # enters 'y' when prompted
+printf 'y\ny\ny\n' | python3 pick_and_place_summit_simulation.py # enters 'y' when prompted
 
 exit 0
