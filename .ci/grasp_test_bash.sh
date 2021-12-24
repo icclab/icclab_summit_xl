d581b337b522602a8f47d67c7439d23abc34b886
Leonardo Militano <39118873+miltzhaw@users.noreply.github.com>
2021-12-24 14:04:26 +0100

    Update grasp_test_bash.sh

--- .ci/grasp_test_bash.sh
+++ .ci/grasp_test_bash.sh
@@ -13,6 +13,8 @@ roslaunch icclab_summit_xl irlab_sim_summit_xls_grasping.launch launch_rviz_gras
 # save PID of roslaunch
 ROSLAUNCH_PID=$!
 
+# let it start completely
+sleep 30
 
 RES=`timeout -k 180s 180s rostopic echo -n 1 "/summit_xl/move_group/status" | grep -c "header"`
 
