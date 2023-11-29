8fa0b5441fe5c900498747e2535beb42229ff441
Giovanni Toffetti <toff@zhaw.ch>
2023-11-29 10:20:08 +0100

    added screen script that connects VPN with multicast and adds ros1 bridge

---
+++ scripts/start_vpn_mc_and_containers.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Start a background screen name the session "summit":
+screen -d -m -S summit
+
+#(Re)Start VPN, enable multicast
+screen -S summit -X exec bash -c "sudo wg-quick down wg0; sudo wg-quick up wg0; sudo ip link set dev wg0 multicast on; sudo wg show"
+
+#Start another window, name it "robot" and run robot startup in it
+screen -S summit -X screen -t robot
+screen -S summit -p robot -X exec roslaunch icclab_summit_xl irlab_summit_xl_amcl.launch sim:=false
+
+#In another window we have the ros1_bridge
+screen -S summit -X screen -t ros1_bridge
+screen -S summit -p ros1_bridge -X exec docker run -it --privileged --network=host --ipc=host --pid=host -v /home/summit:/home/summit ros:galactic-ros1-bridge bash -c "export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp; export CYCLONEDDS_URI=file:////home/summit/cyclonedds-mc-galactic.xml; ros2 run ros1_bridge dynamic_bridge --bridge-all-topics"
+
+#In another window we have the ros2 container that will run nav2
+screen -S summit -X screen -t ros2
+screen -S summit -p ros2 -X exec docker run -it --privileged --network=host --ipc=host --pid=host --env UID=$(id -u) --env GID=$(id -g) -v /home/summit:/home/summit robopaas/rosdocked-humble-cpu bash -c "export ROS_DISTRO=humble; export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp; export CYCLONEDDS_URI=file:////home/summit/cyclonedds-mc.xml; watch ros2 node list"
+
+
+
+
