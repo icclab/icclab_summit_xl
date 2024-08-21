d635c13ecf345b74feaa838f1517659f5c0fa899
Leonardo Militano <39118873+miltzhaw@users.noreply.github.com>
2024-08-21 13:41:18 +0200

    Create map_save.launch.py

---
+++ icclab_summit_xl/launch/map_save.launch.py
@@ -0,0 +1,13 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+ 
+def generate_launch_description():
+    ld = LaunchDescription()
+    map_saver = Node(
+        package="nav2_map_server",
+        executable="map_saver_cli",
+        arguments=['-f', 'my_map'],
+    )
+ 
+    ld.add_action(map_saver)
+    return ld
