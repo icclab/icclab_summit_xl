9113994160013bde59a58bfdcd287eb6a3b32748
Leonardo Militano <39118873+miltzhaw@users.noreply.github.com>
2025-01-20 15:43:39 +0100

    Update move_group_nodisplay.launch.py

--- icclab_summit_xl_move_it_config/launch/move_group_nodisplay.launch.py
+++ icclab_summit_xl_move_it_config/launch/move_group_nodisplay.launch.py
@@ -64,7 +64,7 @@ def generate_move_group_launch(moveit_config):
         parameters=move_group_params,
         extra_debug_args=["--debug"],
         # Set the display variable, in case OpenGL code is used internally
-      #  additional_env={"DISPLAY": ":0"},
+        additional_env={"DISPLAY": ":99"}, #added 99 as workaround display in headless container
     )
     return ld
 
