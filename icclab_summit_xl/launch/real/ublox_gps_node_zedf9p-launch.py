50595b575d57bfbf21d51131a9bd4d92d896d335
Gaurav Kapoor <gkapoordelhi@gmail.com>
2025-01-30 17:02:54 +0000

    added GPS driver launch file

---
+++ icclab_summit_xl/launch/real/ublox_gps_node_zedf9p-launch.py
@@ -0,0 +1,27 @@
+import os
+
+import ament_index_python.packages
+import launch
+import launch_ros.actions
+
+
+def generate_launch_description():
+    config_directory = os.path.join(
+        ament_index_python.packages.get_package_share_directory('icclab_summit_xl'),
+        'config')
+    params = os.path.join(config_directory, 'zed_f9p.yaml')
+    ublox_gps_node = launch_ros.actions.Node(package='ublox_gps',
+                                             executable='ublox_gps_node',
+                                             output='both',
+                                             parameters=[params],
+                                             remappings=[('/fix', '/summit/fix')])
+ 
+    return launch.LaunchDescription([ublox_gps_node,
+ 
+                                     launch.actions.RegisterEventHandler(
+                                         event_handler=launch.event_handlers.OnProcessExit(
+                                             target_action=ublox_gps_node,
+                                             on_exit=[launch.actions.EmitEvent(
+                                                 event=launch.events.Shutdown())],
+                                         )),
+                                     ])
